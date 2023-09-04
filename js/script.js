29bc8acf1d90b7dbb0f2044167f0e7fc3a8f9158
蔡相襄 <78093303+hsiang2@users.noreply.github.com>
2023-09-04 23:45:43 +0800

    Add detail page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-const movieList = document.querySelector('#movie-list')
+// const movieList = document.querySelector('#movie-list')
+const content = document.querySelector('#content')
+
 const homeBtn = document.querySelector('#home')
 
 // read local JSON file in javascript
@@ -7,37 +9,72 @@ fetch("./json/movies.json")
         return response.json();
     })
     .then((json) => {
-        movieList.innerHTML = renderMovieList(json);
+        content.innerHTML = renderMovieList(json);
         
     })
     .catch(err => {
         console.log('Request Failed', err);
     });
 
-// homeBtn.addEventListener("click", () => {
-//     fetch("./json/movies.json")
-//     .then((response) => {
-//         return response.json();
-//     })
-//     .then((json) => {
-//         movieList.innerHTML = renderMovieList(json);
+
+
+homeBtn.addEventListener("click", () => {
+    fetch("./json/movies.json")
+    .then((response) => {
+        return response.json();
+    })
+    .then((json) => {
+        content.innerHTML = renderMovieList(json);
         
-//     })
-//     .catch(err => {
-//         console.log('Request Failed', err);
-//     });
-// })
+    })
+    .catch(err => {
+        console.log('Request Failed', err);
+    });
+})
 
 function renderMovieList(json) {
     return `
+        <section class="movie-list">
         ${json.map(movie => {
             return `
                 <article>
-                    <img src=${movie.image} class="item-img">
-                    <p class="item-name">${movie.name}</>
-                    <p class="item-storyline">${movie.storylineShort}</>
+                    <button data-movie='${JSON.stringify(movie)}' onclick="pressMovie(this)">
+                        <img src=${movie.image} class="item-img">
+                        <p class="item-name">${movie.name}</>
+                        <p class="item-storyline">${movie.storylineShort}</>
+                    </button>
                 </article>
             `
         }).join("")}
+        </section>
+    `
+}
+
+function pressMovie(button) {
+    const movieData = JSON.parse(button.dataset.movie)
+
+    content.innerHTML = `
+        <section class="info">
+            <img src=${movieData.image} class="info-img">
+            <div class="info-div">
+                <h1 class="info-name">${movieData.name}</h1>
+                <p class="info-details">${movieData.year} / ${movieData.rating} / ${movieData.runningTime}</>
+                <p class="info-storyline">${movieData.storylineLong}</>
+            </div>
+        </section>
+        <section class="cast">
+            ${movieData.cast.map(actor => {
+                return `
+                    <div class="actor">
+                        <img src=${actor.image} class="actor-img">
+                        <p class="actor-name">${actor.name}</>
+                        <p class="actor-character">${actor.character}</>
+                    </div>
+                `
+            }).join("")}
+        </section>
+        <section class="reviews">
+        
+        </section>
     `
 }
