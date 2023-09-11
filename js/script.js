b0a542f5a4d9516bdaf4a671e4903f4a957980cc
hsiang2 <q224399377@gmail.com>
2023-09-11 12:52:04 +0800

    Add stars

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // const movieList = document.querySelector('#movie-list')
 const content = document.querySelector('#content')
-
 const homeBtn = document.querySelector('#home')
+
+var reviews = []
 
 // read local JSON file in javascript
 fetch("./json/movies.json")
@@ -58,7 +59,7 @@ function pressMovie(button) {
             <img src=${movieData.image} class="info-img">
             <div class="info-div">
                 <h1 class="info-name">${movieData.name}</h1>
-                <p class="info-details">${movieData.year} / ${movieData.rating} / ${movieData.runningTime}</>
+                <p class="info-details">${movieData.year} . ${movieData.rating} . ${movieData.runningTime}</>
                 <p class="info-storyline">${movieData.storylineLong}</>
             </div>
         </section>
@@ -74,7 +75,67 @@ function pressMovie(button) {
             }).join("")}
         </section>
         <section class="reviews">
-        
+            <h1 class="info-name">Rate and Review</h1>
+            <form onsubmit="submitReview(this);return false">
+                <p class="info-details">Rate this movie on a scale of 1-5.</p>
+                <div class="slider">
+                    <input 
+                        type="range" 
+                        id="rating" name="rating"
+                        min="0" max="5" 
+                        step="0.5" value="0" 
+                        oninput="rangeValue.innerText = this.value"
+                    >
+                    <p id="rangeValue">0</p>
+                </div> 
+                <p class="info-details">Write a review.</p>
+                <textarea class="text-input" rows="8" name="review"></textarea>
+                <input class="btn" type="submit" value="SUBMIT">
+            </form>
+        </section>
+        <section class="reviews">
+            <h1 class="info-name">Reviews</h1>
+            <div id="reviews-list"></div>
         </section>
     `
+}
+
+// ${reviews.map(review => {
+//     return `
+//         <div>
+//             <p>${review.score}</p>
+//             <p>${review.comment}</p>
+//         </div>
+//     `
+// }).join("")}
+
+function submitReview(form) {
+    reviews.push({"score": form.rating.value, "comment": form.review.value})
+
+
+    document.getElementById("reviews-list").innerHTML = `
+        ${reviews.map(review => {
+            return `
+                <article class="review-item">
+                    <div>
+                        <i class="${review.score >= 1 ? "fa-solid fa-star" : review.score >= 0.5 ? "fa-solid fa-star-half-stroke" : "fa-regular fa-star"}"></i>
+                        <i class="${review.score >= 2 ? "fa-solid fa-star" : review.score >= 1.5 ? "fa-solid fa-star-half-stroke" : "fa-regular fa-star"}"></i>
+                        <i class="${review.score >= 3 ? "fa-solid fa-star" : review.score >= 2.5 ? "fa-solid fa-star-half-stroke" : "fa-regular fa-star"}"></i>
+                        <i class="${review.score >= 4 ? "fa-solid fa-star" : review.score >= 3.5 ? "fa-solid fa-star-half-stroke" : "fa-regular fa-star"}"></i>
+                        <i class="${review.score >= 5 ? "fa-solid fa-star" : review.score >= 4.5 ? "fa-solid fa-star-half-stroke" : "fa-regular fa-star"}"></i>
+                    </div>
+                    <p>${review.comment}</p>
+                </article>
+            `
+        }).join("")}
+    `
+    // document.getElementById("reviews-list").innerHTML = `
+    //         <p>${form.rating.value}</p>
+    //         <p>${form.review.value}</p>
+    //     `
+    form.rating.value = 0
+    rangeValue.innerText = 0
+    form.review.value = ""
+   
+
 }
